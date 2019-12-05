6f03c6013560873bb60da6ddfcac0b4d1a7ea1ca
Evgeny Murashov <github.com/evgenymoore>
2019-12-05 09:16:54 +0300

    adding new files with function description, peripherial settings

---
+++ Inc/adc_controller.h
@@ -0,0 +1,4 @@
+#ifndef __ADC_CONTROLLER_H
+#define __ADC_CONTROLLER_H
+
+#endif /* __ADC_CONTROLLER_H */
---
+++ Inc/main_loop.h
@@ -0,0 +1,5 @@
+#ifndef __MAIN_LOOP_H
+#define __MAIN_LOOP_H
+
+
+#endif /* __MAIN_LOOP_H */
